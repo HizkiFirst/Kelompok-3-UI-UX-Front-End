fb704a4fea2c6f79e8a799812036eacef05f6890
NathanAr42 <92615646+NathanAr42@users.noreply.github.com>
2021-10-16 17:13:07 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ class perBuku extends HTMLElement{
     this.innerHTML = `
     <style>
     #wrapper {
-      margin-left:10px;
+      margin-left:30px;
       border:hide;
       width: 630px;
       height:400px;
